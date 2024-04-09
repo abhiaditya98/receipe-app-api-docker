4746abc5282a1d97cd0e337d854cd60b3f0c9386
Aditya Abhi <abhiaditya98@gmail.com>
2024-04-09 18:27:20 +0530

    User API rectified and Updated

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -7,14 +7,13 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializers for the user object"""
+    """Serializer for the user object."""
 
     class Meta:
-        model=get_user_model()
-        fields = ['email','password','none']
-        extra_kwargs = {'password':{'write_only':True,'min_length':3}}
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-        def create(self,validated_data):
-            """Create and return a user with encrypted password"""
-
-            return get_user_model().objects.create_user(**validated_data)
+    def create(self, validated_data):
+        """Create and return a user with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
--- app/user/views.py
+++ app/user/views.py
@@ -9,5 +9,4 @@ from user.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
 
-    seriializer_class = UserSerializer
-    
+    serializer_class = UserSerializer
